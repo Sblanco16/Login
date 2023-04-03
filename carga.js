3b8ffe040619ac31e014c4410bacebee87e28571
Vidal <diana-vidal-10@hotmail.com>
2023-04-03 15:47:49 -0500

    Validaciones

--- carga.js
+++ carga.js
@@ -1,7 +1,7 @@
 let tabla = [
-    { identificacion:"1002198275", nombre1: "Pedro", nombre2:"Luis", apellido1: "Garcia", apellido2: "Gomez", edad:"30",direccion: "calle 30", correo:"pedro_garcia@gmail.com" },
-    { identificacion:"1908767589",nombre1: "Juan", nombre2:"Daniel", apellido1: "Morelo", apellido2: "Lopez", edad:"22",direccion: "calle 40", correo:"Juan_Morelo@gmail.com" },
-    { identificacion:"2667894013",nombre1: "Ana", nombre2:"Maria", apellido1: "Hernandez", apellido2: "Cañate", edad:"28",direccion: "calle 50", correo:"Ana_Hernandez@gmail.com" },
+    { identificacion: "1002198275", nombre1: "Pedro", nombre2: "Luis", apellido1: "Garcia", apellido2: "Gomez", edad: "30", direccion: "calle 30", correo: "pedro_garcia@gmail.com" },
+    { identificacion: "1908767589", nombre1: "Juan", nombre2: "Daniel", apellido1: "Morelo", apellido2: "Lopez", edad: "22", direccion: "calle 40", correo: "Juan_Morelo@gmail.com" },
+    { identificacion: "2667894013", nombre1: "Ana", nombre2: "Maria", apellido1: "Hernandez", apellido2: "Cañate", edad: "28", direccion: "calle 50", correo: "Ana_Hernandez@gmail.com" },
 ];
 
 let cuerpotabla = document.getElementById("tabla-usuario")
@@ -23,33 +23,44 @@ function listar() {
     cuerpotabla.innerHTML = listado
 }
 
-    function nuevo(){
-        let cedula = document.getElementById("cedula").value;
-        let nombre = document.getElementById("nombre").value;
-        let nombre2 = document.getElementById("nombre2").value;
-        let apellido = document.getElementById("apellido").value;
-        let apellido2 = document.getElementById("apellido2").value;
-        let edad = document.getElementById("edad").value;
-        let direccion = document.getElementById("direccion").value;
-        let correo = document.getElementById("correo").value;
+function nuevo() {
+    let cedula = document.getElementById("cedula").value;
+    let nombre1 = document.getElementById("nombre").value;
+    let nombre2 = document.getElementById("nombre2").value;
+    let apellido1 = document.getElementById("apellido").value;
+    let apellido2 = document.getElementById("apellido2").value;
+    let edad = document.getElementById("edad").value;
+    let direccion = document.getElementById("direccion").value;
+    let correo = document.getElementById("correo").value;
 
-        if(cedula =="" || nombre =="" ||  nombre2 =="" || apellido =="" || apellido2=="" || edad=="" || direccion=="" || correo==""){
-            swal.fire({
-                title: "Error!",
-                text: "Por favor rectifique haber llenado todos los campos",
-                icon:"error",
-                allowDutsideClick: true,
-                allowEscapeKey: false,
-                allowEnterKey: false,
-                stopKeydownPropagation: true
-        
-            });} else{
+    if (cedula == "" || nombre1 == "" || nombre2 == "" || apellido1 == "" || apellido2 == "" || edad == "" || direccion == "" || correo == "") {
+        swal.fire({
+            title: "Error!",
+            text: "Por favor rectifique haber llenado todos los campos.",
+            icon: "error",
+            allowDutsideClick: true,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+            stopKeydownPropagation: true
 
-        cuerpotabla.innerHTML +=  `<tr>
+        });
+    } else if (validarcc(cedula)) {
+        swal.fire({
+            title: "Error!",
+            text: "La cédula " +cedula+ " ya se encuentra registrada.",
+            icon: "error",
+            allowDutsideClick: true,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+            stopKeydownPropagation: true
+
+        });}else{
+
+        cuerpotabla.innerHTML += `<tr>
         <th scope="row">${cedula}</th>
-        <td>${nombre}</td>
+        <td>${nombre1}</td>
         <td>${nombre2}</td>
-        <td>${apellido}</td>
+        <td>${apellido1}</td>
         <td>${apellido2}</td>
         <td>${edad}</td>
         <td>${direccion}</td>
@@ -58,16 +69,31 @@ function listar() {
 
 
         let nuevo = {
-            cedula: cedula,
-            nombre: nombre,
+            identificacion: cedula,
+            nombre1: nombre1,
             nombre2: nombre2,
-            apellido: apellido,
+            apellido1: apellido1,
             apellido2: apellido2,
             edad: edad,
             direccion: direccion,
             correo: correo
-            }
+        }
+
+        Swal.fire(
+            'Registro exitoso!',
+            'El usuario se ha registrado correctamente.',
+            'success'
+          )
 
         tabla.push(nuevo)
-            }
     }
+}
+
+function validarcc(cedula) {
+    for (let index = 0; index < tabla.length; index++) {
+        if (tabla[index].identificacion == cedula) {
+            return true;
+        }
+    }
+    return false;
+}
